a437e025baf62bb52da51971ec609d1782f60a73
Emma Buller <dib5017@gmail.com>
2021-01-04 17:13:10 -0500

    Tested pigLatinBest

--- PigLatin.java
+++ PigLatin.java
@@ -11,4 +11,29 @@ public class PigLatin {
       return ls.substring(1) + ls.charAt(0) + "ay";
     }
   }
+  public static String pigLatin(String s) {
+    String ls = s.toLowerCase();
+    ArrayList<String> diagraphs = new ArrayList<String>(Arrays.asList(new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"}));
+    if(diagraphs.contains(ls.substring(0,2))) {
+      return ls.substring(2) + ls.substring(0,2) + "ay";
+    }
+    else {
+      return pigLatinSimple(ls);
+    }
+  }
+  public static String pigLatinBest(String s) {
+    String ls = s.toLowerCase();
+    ArrayList<String> numbers = new ArrayList<String>(Arrays.asList(new String[]{"0","1","2","3","4","5","6","7","8","9"}));
+    ArrayList<String> letters = new ArrayList<String>(Arrays.asList(new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"}));
+    if(!(letters.contains(ls.substring(0,1)))) {
+      return ls;
+    }
+    if(letters.contains(ls.substring(ls.length()-1)) || numbers.contains(ls.substring(ls.length()-1))) {
+      return pigLatin(ls);
+    }
+    else {
+      String sl = s.substring(0,ls.length()-1);
+      return pigLatin(sl) + ls.substring(ls.length()-1);
+    }
+  }
 }
--- plTest.java
+++ plTest.java
@@ -7,5 +7,28 @@ public class plTest {
     System.out.println(PigLatin.pigLatinSimple("aaron"));
     System.out.println(PigLatin.pigLatinSimple("the"));
     System.out.println(PigLatin.pigLatinSimple("check"));
+    System.out.println();
+
+    System.out.println("TESTING pigLatin");
+    System.out.println(PigLatin.pigLatin("the"));
+    System.out.println(PigLatin.pigLatin("check"));
+    System.out.println(PigLatin.pigLatin("skee"));
+    System.out.println(PigLatin.pigLatin("emu"));
+    System.out.println(PigLatin.pigLatin("grade"));
+    System.out.println();
+
+    System.out.println("TESTING pigLatinBest");
+    System.out.println(PigLatin.pigLatinBest("*emu"));
+    System.out.println(PigLatin.pigLatinBest("4chan"));
+    System.out.println(PigLatin.pigLatinBest("fish!"));
+    System.out.println(PigLatin.pigLatinBest("fish"));
+    System.out.println(PigLatin.pigLatinBest("the."));
+    System.out.println(PigLatin.pigLatinBest("cat!"));
+    System.out.println(PigLatin.pigLatinBest("amazing?"));
+    System.out.println(PigLatin.pigLatinBest("apple%"));
+    System.out.println(PigLatin.pigLatinBest("33ls"));
+    System.out.println(PigLatin.pigLatinBest("appl3"));
+    System.out.println(PigLatin.pigLatinBest("skee!"));
+    System.out.println(PigLatin.pigLatinBest("5kee"));
   }
 }
